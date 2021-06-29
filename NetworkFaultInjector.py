878337f308053e719de63a4e18d72eae16fb8730
GabrieleGavarini <gabrielegavarini@gmail.com>
2021-06-29 20:17:04 +0200

    - Created a separate function to generate fault list - Added test in main - Added exception if number of fault exceed fault list length

--- NetworkFaultInjector.py
+++ NetworkFaultInjector.py
@@ -2,46 +2,85 @@ import numpy as np
 from FaultInjector import FaultInjector
 
 
+class OutOfFaultList(Exception):
+    pass
+
+
 class NetworkFaultInjector:
 
     def __init__(self, network, seed):
+        """
+            Initialize the fault injector
+            :param network: the network to be injected
+            :param seed: the seed of the run
+        """
         self.network = network
         self.seed = seed
 
-        self.rng = np.random.default_rng(seed=self.seed)
+        self.rng = np.random.default_rng(seed=self.seed)    # The dedicated random number generator
 
-    def bit_flip_injection_campaign(self, number_faults):
+        self.layer_shape = [(layer.get_weights()[0].shape if (len(layer.get_weights()) > 0) else ())
+                            for layer in self.network.layers]   # List of the shape of all layers
+
+        self.fault_list = []           # The list containing all the fault generated for this run
+        self.index_last_injection = 0  # The fault list index of the last fault injected
+
+    def generate_bit_flip_fault_list(self, fault_list_length=10000):
         """
-        Run a fault injection campaign
-        :param number_faults: The number of faults to inject
-        :return:
+        Generate and set the fault list. Each entry of the fault list is a list of three elements: the first element is
+        the index of the layer, the second is a tuple containing the index of the weight, the last element is the bit to
+        be flipped
+        :param fault_list_length: Length of the fault list
         """
         layer_parameters = [layer.count_params() for layer in self.network.layers]
         layer_probabilities = layer_parameters / np.sum(layer_parameters)
 
         # Generate the probability for the layer based on the number of parameters
         target_layers = self.rng.choice(len(layer_probabilities),
-                                        size=number_faults,
+                                        size=fault_list_length,
                                         replace=True,
                                         p=layer_probabilities)
 
+        # For each layer selected, generate an injection index and a target bit
+        for layer_index in target_layers:
+            injection_index = tuple([self.rng.integers(0, i) for i in self.layer_shape[layer_index]])
+            self.fault_list.append([layer_index, injection_index, self.rng.integers(0, 32)])
+
+    def bit_flip_increment(self, increment_number):
+        """
+        Inject new faults on top of those already present in the network. Fault injections are done layer by layer (i.e.
+        we cycle trough all the layer and for each one of them we inject the corresponding fault from the incremental
+        fault list). The update of the network weights is done once per layer.
+        :param increment_number: The number of faults to inject on top of those already present
+        :return:
+        """
+
+        incremental_index = self.index_last_injection + increment_number
+        if incremental_index > len(self.fault_list):
+            raise OutOfFaultList('The index of the incremental is larger than the dimension of the fault list')
+
+        target_list = np.array(self.fault_list[self.index_last_injection: incremental_index])
+        self.index_last_injection = incremental_index
+
         # Cycle trough all the selected layer
-        for layer_info in zip(*np.unique(target_layers, return_counts=True)):
-            layer_index = layer_info[0]     # Layer index
-            layer_count = layer_info[1]     # How many fault to inject in this layer
+        for layer_info in zip(*np.unique(target_list[:, 0], return_counts=True)):
+            layer_index = layer_info[0]  # Layer index
+            layer_count = layer_info[1]  # How many fault to inject in this layer
 
-            layer = self.network.layers[layer_index]
+            # Get the list of faults for the current layer
+            fault_for_layer = target_list[np.where(target_list[:, 0] == layer_index)]
 
-            weights = layer.get_weights()[0]
-            # TODO: inject also on bias
-            bias = layer.get_weights()[1]
+            weights = self.network.layers[layer_index].get_weights()[0]
+            bias = self.network.layers[layer_index].get_weights()[1]
 
-            for _ in np.arange(0, layer_count):
+            for i in np.arange(0, layer_count):
                 # Get the index of the weight to inject
-                injection_index = tuple([self.rng.integers(0, i) for i in weights.shape])
+                injection_index = fault_for_layer[i][1]
+                # Get which bit to flip
+                injection_position = fault_for_layer[i][2]
                 # Perform the fault injection
                 weights[injection_index] = FaultInjector.float32_bit_flip(float_number=weights[injection_index],
-                                                                          position=self.rng.integers(0, 32))
+                                                                          position=injection_position)
 
             # Update the weight with the faulty value
             self.network.layers[layer_index].set_weights((weights, bias))
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from tensorflow.keras.applications import VGG16
 from NetworkFaultInjector import NetworkFaultInjector
 
+if __name__ == "__main__":
+    vgg = VGG16()
 
-vgg = VGG16()
+    fault_injector = NetworkFaultInjector(vgg, 11234)
+    fault_injector.generate_bit_flip_fault_list()
 
-fault_injector = NetworkFaultInjector(vgg, 11234)
-fault_injector.bit_flip_injection_campaign(10000)
+    first_fault = fault_injector.fault_list[0]
+    print(f'Value before the injection: {vgg.layers[first_fault[0]].get_weights()[0][first_fault[1]]}')
+    fault_injector.bit_flip_increment(1000)
+    print(f'Value after the injection: {vgg.layers[first_fault[0]].get_weights()[0][first_fault[1]]}')
