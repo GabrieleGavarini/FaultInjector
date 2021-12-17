83fb1c7bbfdb11ed75c7893f5d5f4c0f1a7e430c
GabrieleGavarini <gabrielegavarini@gmail.com>
2021-12-17 11:47:54 +0100

    - Changed requirement - Added new script for generation of fault list

--- FaultInjector/StuckAtFaultInjector.py
+++ FaultInjector/StuckAtFaultInjector.py
@@ -1,7 +1,9 @@
 from FaultInjector.NetworkFaultInjector import NetworkFaultInjector
 from FaultInjector.FaultInjectorEngine import FaultInjectorEngine
 
+from tqdm import tqdm
 import numpy as np
+import itertools
 
 
 class StuckAtFaultInjector(NetworkFaultInjector):
@@ -19,26 +21,72 @@ class StuckAtFaultInjector(NetworkFaultInjector):
 
         target_layers = super().generate_layer_probability(fault_list_length)
 
-        # For each layer selected, generate an injection index and a target bit
-        for layer_index in target_layers:
-            while True:
-                # Get the probability of a fault affecting the bias versus the weights
-                total_layer_params = self.network.layers[layer_index].get_weights()[0].size +\
-                                     self.network.layers[layer_index].get_weights()[1].size
-                weights_probability = self.network.layers[layer_index].get_weights()[0].size / total_layer_params
-                bias_or_weights = self.rng.choice([0, 1], p=[weights_probability, 1 - weights_probability])
-                # Where to inject the fault
-                if bias_or_weights == 0:
-                    injection_index = tuple([self.rng.integers(0, i) for i in self.layer_shape[layer_index]])
-                else:
-                    injection_index = tuple([self.rng.integers(self.network.layers[layer_index].get_weights()[1].size)])
-                # Value to inject
-                stuck_at_value = self.rng.integers(0, 1, endpoint=True)
-                # Compose the fault details in a list
-                fault_list_element = [layer_index, bias_or_weights, injection_index, self.rng.integers(0, 32), stuck_at_value]
-                if fault_list_element not in self.fault_list:
-                    break
-            self.fault_list.append(fault_list_element)
+        # Generate a dictionary containing the number of fault to generate in each layer
+        layers_list, layers_count = np.unique(target_layers, return_counts=True)
+        layers_dict = dict(zip(layers_list, layers_count))
+
+        for layer_index, layer_count in tqdm(layers_dict.items()):
+
+            weight_params = self.network.layers[layer_index].get_weights()[0].size
+            bias_params = self.network.layers[layer_index].get_weights()[1].size
+
+            total_layer_params = weight_params + bias_params
+            weights_probability = weight_params / total_layer_params
+            weight_count = int(np.ceil(weights_probability * layer_count))
+            bias_count = layer_count - weight_count
+
+            if len(self.network.layers[layer_index].get_weights()[0].shape) == 4:
+                dim_0 = np.arange(self.network.layers[layer_index].get_weights()[0].shape[0])
+                dim_1 = np.arange(self.network.layers[layer_index].get_weights()[0].shape[1])
+                dim_2 = np.arange(self.network.layers[layer_index].get_weights()[0].shape[2])
+                dim_3 = np.arange(self.network.layers[layer_index].get_weights()[0].shape[3])
+                layer_weights_locations = tuple(itertools.product(*[dim_0, dim_1, dim_2, dim_3]))
+            else:
+                dim_0 = np.arange(self.network.layers[layer_index].get_weights()[0].shape[0])
+                dim_1 = np.arange(self.network.layers[layer_index].get_weights()[0].shape[1])
+                layer_weights_locations = tuple(itertools.product(*[dim_0, dim_1]))
+
+            layer_weight_fault_locations = [tuple(location) for location in self.rng.choice(layer_weights_locations,
+                                                                                            size=min(len(layer_weights_locations),
+                                                                                                     weight_count),
+                                                                                            replace=False)]
+
+            layer_bias_fault_locations = [tuple(location) for location in self.rng.choice(np.arange(bias_params),
+                                                                                          size=min(bias_params,
+                                                                                                   bias_count),
+                                                                                          replace=False)]
+
+            layer_fault_locations = layer_weight_fault_locations + layer_bias_fault_locations
+
+            bias_or_weights = list(np.full(len(layer_weight_fault_locations), 0)) + list(np.full(len(layer_bias_fault_locations), 1))
+            layer = np.full(len(layer_fault_locations), layer_index)
+            bits = self.rng.integers(32, size=len(layer_fault_locations))
+            values = self.rng.integers(2, size=len(layer_fault_locations))
+
+            layer_faults = list(zip(layer, bias_or_weights, layer_fault_locations, bits, values))
+            self.fault_list += layer_faults
+            pass
+
+        # # For each layer selected, generate an injection index and a target bit
+        # for layer_index in target_layers:
+        #     while True:
+        #         # Get the probability of a fault affecting the bias versus the weights
+        #         total_layer_params = self.network.layers[layer_index].get_weights()[0].size +\
+        #                              self.network.layers[layer_index].get_weights()[1].size
+        #         weights_probability = self.network.layers[layer_index].get_weights()[0].size / total_layer_params
+        #         bias_or_weights = self.rng.choice([0, 1], p=[weights_probability, 1 - weights_probability])
+        #         # Where to inject the fault
+        #         if bias_or_weights == 0:
+        #             injection_index = tuple([self.rng.integers(0, i) for i in self.layer_shape[layer_index]])
+        #         else:
+        #             injection_index = tuple([self.rng.integers(self.network.layers[layer_index].get_weights()[1].size)])
+        #         # Value to inject
+        #         stuck_at_value = self.rng.integers(0, 1, endpoint=True)
+        #         # Compose the fault details in a list
+        #         fault_list_element = [layer_index, bias_or_weights, injection_index, self.rng.integers(0, 32), stuck_at_value]
+        #         if fault_list_element not in self.fault_list:
+        #             break
+        #     self.fault_list.append(fault_list_element)
 
     def inject_incremental_fault(self, increment_number):
         """
